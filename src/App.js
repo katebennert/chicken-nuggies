044901b5f2299b629ce7a905afa893a91b7718a5
Kate Bennert <kate.bennert@gmail.com>
2023-11-01 16:27:19 -0400

    Add Nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./Home";
+import Nav from "./Nav";
 import Scroll from "./Scroll";
 
 function App() {
   return (
     <div className="font-inter">
       <Home />
+      <Nav />
       <Scroll />
-
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <div className="">
+            <h3>About</h3>
+            <h3>Experience</h3>
+            <h3>Projects</h3>
+        </div>
+    );
+  }
+  
+  export default Nav;
+  
