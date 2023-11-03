46adf2bcf0fd7531bce9282fb2055befeec179ac
Kate Bennert <kate.bennert@gmail.com>
2023-11-03 15:19:57 -0400

    Start sprinkle animation

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
+import Sprinkles from "./Sprinkles";
+
 function Home() {
+
+    const [animationActive, setAnimationActive] = useState(false);
+
+    const startAnimation = () => {
+      setAnimationActive(true);
+      console.log("started")
+      const sprinkleJar = document.querySelector('.sprinkle-jar');
+      sprinkleJar.classList.add('animate-sprinkles');
+  
+      setTimeout(() => {
+        setAnimationActive(false);
+        console.log("stopped")
+      }, 5000); 
+    };
+
     return (
         <div className="flex flex-col justify-center items-center">
-            <h1 className="font-limelight text-pink-500 text-4xl md:text-5xl xl:text-6xl lg:mb-4 ">Kate Bennert</h1>
+            <div className="sprinkle-jar">
+                {animationActive && <div className="animate-sprinkles">
+                    <Sprinkles />
+                </div>}
+            </div>
+            <span className="font-limelight text-pink-500 text-4xl md:text-5xl xl:text-6xl lg:mb-4" onMouseEnter={startAnimation}>Kate Bennert</span>
             <h2 className="mb-4">Creative Software Engineer and Web Designer</h2>
             <h3>TK description</h3>
         </div>
---
+++ src/Sprinkles.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Sprinkles = () => {
+
+  return (
+    <div>
+      Sprinkle
+    </div>
+  );
+};
+
+export default Sprinkles;
