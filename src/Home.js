c319754c432a3b5e3e89eace09831e6c1fe6b59a
Kate Bennert <kate.bennert@gmail.com>
2023-11-03 15:33:10 -0400

    Reset sprinkles

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,8 @@ function Home() {
     const startAnimation = () => {
       setAnimationActive(true);
       console.log("started")
-      const sprinkleJar = document.querySelector('.sprinkle-jar');
-      sprinkleJar.classList.add('animate-sprinkles');
+    //   const sprinkleJar = document.querySelector('.sprinkle-jar');
+    //   sprinkleJar.classList.add('animate-sprinkles');
   
       setTimeout(() => {
         setAnimationActive(false);
@@ -20,9 +20,9 @@ function Home() {
     return (
         <div className="flex flex-col justify-center items-center">
             <div className="sprinkle-jar">
-                {animationActive && <div className="animate-sprinkles">
-                    <Sprinkles />
-                </div>}
+                <div className={animationActive ? "animate-sprinkles" : ""}>
+                    {animationActive && <Sprinkles />}
+                </div>
             </div>
             <span className="font-limelight text-pink-500 text-4xl md:text-5xl xl:text-6xl lg:mb-4" onMouseEnter={startAnimation}>Kate Bennert</span>
             <h2 className="mb-4">Creative Software Engineer and Web Designer</h2>
