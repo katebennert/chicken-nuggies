1ac5d09f2a91f518500213802bf939f9f9000585
Kate Bennert <kate.bennert@gmail.com>
2023-11-03 14:15:15 -0400

    Fix right side spacing

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
           <div className="border border-green-500 mb-6 mt-10 "> {/* HOME */}
             <Home />
           </div>
-          <div className="hidden lg:block border border-yellow-500 mb-6 "> {/* NAV */}
+          <div className="hidden lg:block border border-yellow-500 mb-6 lg:mt-20"> {/* NAV */}
             <Nav />
           </div>
-          <div className="border border-pink-500 flex flex-row space-x-6 lg:space-x-10 mb-6 lg:justify-center">
+          <div className="border border-pink-500 flex flex-row space-x-6 lg:space-x-10 mb-6 justify-center lg:mt-10 lg:mb-20">
             <FontAwesomeIcon icon={faGithub} style={{ fontSize: '1.5rem' }} />
             <FontAwesomeIcon icon={faXTwitter} style={{ fontSize: '1.5rem' }} />
             <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '1.5rem' }} />
@@ -24,7 +24,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="flex-grow order-2 pl-6 pr-6 md:pt-10 md:pr-10 md:pl-10 lg:pt-20 lg:w-1/2"> {/* RIGHT SIDE SCROLL*/}
+      <div className="flex-grow order-2 p-6 md:p-10 lg:w-1/2 lg:p-20 "> {/* RIGHT SIDE SCROLL*/}
         <Experience />
       </div>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="flex flex-col justify-center items-center">
-            <h1 className="font-limelight text-pink-500 text-4xl md:text-5xl xl:text-6xl ">Kate Bennert</h1>
+            <h1 className="font-limelight text-pink-500 text-4xl md:text-5xl xl:text-6xl lg:mb-4 ">Kate Bennert</h1>
             <h2 className="mb-4">Creative Software Engineer and Web Designer</h2>
             <h3>TK description</h3>
         </div>
