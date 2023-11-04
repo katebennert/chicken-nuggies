f8969ef7c0f1a0ed02218149007652db8e02cc13
Kate Bennert <kate.bennert@gmail.com>
2023-11-03 21:51:10 -0400

    Create sprinkles

--- src/Sprinkles.js
+++ src/Sprinkles.js
@@ -3,8 +3,13 @@ import React from 'react';
 function Sprinkles() {
 
   return (
-    <span className="sprinkle-jar border border-yellow-600">
-      Sprinkle eeeeeee
+    <span className="sprinkle-jar flex flex-row justify-between space-x-20">
+      <div className="sprinkle-pink"></div>
+      <div className="sprinkle-yellow"></div>
+      <div className="sprinkle-green"></div>
+      <div className="sprinkle-orange"></div>
+      <div className="sprinkle-white"></div>
+      <div className="sprinkle-purple"></div>
     </span>
   );
 };
