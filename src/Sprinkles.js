9e64cdc24f8f91a6e308614c5aed38865e39e77c
Kate Bennert <kate.bennert@gmail.com>
2023-11-03 21:52:31 -0400

    Double sprinkles

--- src/Sprinkles.js
+++ src/Sprinkles.js
@@ -10,6 +10,12 @@ function Sprinkles() {
       <div className="sprinkle-orange"></div>
       <div className="sprinkle-white"></div>
       <div className="sprinkle-purple"></div>
+      <div className="sprinkle-pink"></div>
+      <div className="sprinkle-yellow"></div>
+      <div className="sprinkle-green"></div>
+      <div className="sprinkle-orange"></div>
+      <div className="sprinkle-white"></div>
+      <div className="sprinkle-purple"></div>
     </span>
   );
 };
